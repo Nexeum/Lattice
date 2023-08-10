6b1d599af68cc05e5c459e36a52f4065c68ee80c
J0J1-ALT <davidpiino13@gmail.com>
2023-08-09 20:39:44 -0500

    New update

--- myfiles.php
+++ myfiles.php
@@ -5,28 +5,26 @@
     include "aside.php"; 
 ?>
 <?php 
-    $alphabeth ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWYZ1234567890_-";
+    $alphabeth = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWYZ1234567890_-";
     $token = "";
-    for($i=0;$i<6;$i++){
-        $token .= $alphabeth[rand(0,strlen($alphabeth)-1)];
+    for ($i = 0; $i < 6; $i++) {
+        $token .= $alphabeth[rand(0, strlen($alphabeth) - 1)];
     }
-    $_SESSION["tkn"]=$token;
-    $folder=null;
-    if(isset($_GET["folder"]) && $_GET["folder"]!=""){
-        
-        $id_folder=$_GET["folder"];
-        $folder = mysqli_query($con,"select * from file where code=\"$id_folder\"");
-
-        while ($row=mysqli_fetch_array($folder)) {
-            $file_id_folder=$row['id']; 
-            $file_folder_id=$row['folder_id']; 
-            $file_folder_filename=$row['filename'];
-            $file_folder_code=$row['code'];
+    $_SESSION["tkn"] = $token;
+    $folder = null;
+    if (isset($_GET["folder"]) && $_GET["folder"] != "") {
+    
+        $id_folder = $_GET["folder"];
+        $folder = mysqli_query($con, "select * from file where code=\"$id_folder\"");
+    
+        while ($row = mysqli_fetch_array($folder)) {
+            $file_id_folder = $row['id'];
+            $file_folder_id = $row['folder_id'];
+            $file_folder_filename = $row['filename'];
+            $file_folder_code = $row['code'];
         }
     }
-
 ?>
-
     <div class="content-wrapper"><!-- Content Wrapper. Contains page content -->
         <section class="content-header"><!-- Content Header (Page header) -->
             <h1>Mis Archivos </h1>
@@ -34,7 +32,7 @@
                 <li><a href="home.php"><i class="fa fa-dashboard"></i> Home</a></li>
                 <li class="active"><a href="myfiles.php"><i class="fa fa-archive"></i> Mis Archivos</a></li>
                 <?php
-                    if(@mysqli_num_rows($folder)!=0){
+                    if (($folder) && (mysqli_num_rows($folder) != 0)) {
                         echo '<li class="active"><a href="myfiles.php?folder='.$file_folder_code.'"><i class="fa fa-folder-open"></i> '.$file_folder_filename.'</a></li>';
                     }
                 ?>
@@ -45,30 +43,28 @@
             <div class="row"><!-- Small boxes (Stat box) -->
                 <div class="col-md-12">
                 <?php
-                    $files = null;
-                    if(@mysqli_num_rows($folder)==0){
-                        if(isset($_GET["q"]) && $_GET["q"]!=""){
-                            $q=$_GET["q"];
-                            $files = mysqli_query($con,"select * from file where user_id=$my_user_id and folder_id is NULL and (filename like '%$q%' or description like '%$q%') order by is_folder desc, filename asc");
-                           
-                    }else{
-                        $files = mysqli_query($con,"select * from file where user_id=$my_user_id and folder_id is NULL order by is_folder desc, filename asc");
-                    }
-
-                    }else{
-                        // search
-                        if(isset($_GET["q"]) && $_GET["q"]!=""){
-                            $q=$_GET["q"];
-                            $files=mysqli_query($con,"select * from files where folder_id=$file_id_folder and  (filename like '%$q%' or description like '%$q%') order by created_at desc");
-                        }else{
-                            // folder/folder/file.php
-                            $files=mysqli_query($con,"select * from file where folder_id=$file_id_folder order by created_at desc");
-
-                        }
-                    }
-                ?>
-
-
+                        $files = mysqli_query($con, "select * from file where user_id = $my_user_id");
+                            if (($folder) && (mysqli_num_rows($folder) == 0)) {
+                                if (isset($_GET["q"]) && $_GET["q"] != "") {
+                                    $q = $_GET["q"];
+                                    $files = mysqli_query($con, "select * from file where user_id=$my_user_id and folder_id is NULL and (filename like '%$q%' or description like '%$q%') order by is_folder desc, created_at desc");
+                                } else {
+                                    $files = mysqli_query($con, "select * from file where user_id=$my_user_id and folder_id is NULL order by is_folder desc, created_at desc");
+                                }
+                            } else {
+                                // search
+                                if(isset($file_id_folder)) {
+                                    if (isset($_GET["q"]) && $_GET["q"] != "") {
+                                        $q = $_GET["q"];
+                                        $files = mysqli_query($con, "select * from file where folder_id=$file_id_folder and  (filename like '%$q%' or description like '%$q%') order by created_at desc");
+                                    } else {
+                                        // folder/folder/file.php
+                                        $files = mysqli_query($con, "select * from file where folder_id=$file_id_folder order by created_at desc");
+                                    }
+                                }
+                            }
+                        ?>
+                    
                <?php if(isset($_GET["folder"]) && $_GET["folder"]!="" && mysqli_num_rows($folder)==0):?>
                     <p class="alert alert-danger">Estas intentando acceder a una carpeta que no existe!</p>
                 <?php endif; ?>
@@ -94,7 +90,7 @@
 
                     <div class="box">
                         <div class="box-header">
-                            <?php if(@mysqli_num_rows($folder)==0):?>
+                            <?php if(($folder) && mysqli_num_rows($folder)==0):?>
                             <h3 class="box-title">Mis Archivos <i class="fa fa-file"></i></h3>
                             <?php else:?>
                             <h3 class="box-title"><?php echo $file_folder_filename;?> <i class="fa fa-folder"></i></h3>
